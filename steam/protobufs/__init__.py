07934887ff6314af5b3cde4a561b4a933689f55e
James <gobot1234yt@gmail.com>
2020-03-25 19:29:28 +0000

    Protobufs for CM

---
+++ steam/protobufs/__init__.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+
+"""
+MIT License
+
+Copyright (c) 2015 Rossen Georgiev <rossen@rgp.io>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+This is a copy of https://github.com/ValvePython/steam/blob/master/steam/enums/emsg.py
+"""
+
+import fnmatch
+
+from . import steammessages_base_pb2 as message_base
+from . import steammessages_clientserver_2_pb2 as client_server_2
+from . import steammessages_clientserver_friends_pb2 as client_server_friends
+from . import steammessages_clientserver_login_pb2 as client_server_login
+from . import steammessages_clientserver_pb2 as client_server
+from .emsg import EMsg
+from .headers import MsgHdr, ExtendedMsgHdr, MsgHdrProtoBuf, GCMsgHdr, GCMsgHdrProto
+from .structs import get_struct
+from .unified import get_um
+
+cmsg_lookup_predefined = {
+    EMsg.Multi: message_base.CMsgMulti,
+    EMsg.ClientToGC: client_server_2.CMsgGCClient,
+    EMsg.ClientFromGC: client_server_2.CMsgGCClient,
+    EMsg.ClientServiceMethod: client_server_2.CMsgClientServiceMethodLegacy,
+    EMsg.ClientServiceMethodResponse: client_server_2.CMsgClientServiceMethodLegacyResponse,
+    EMsg.ClientGetNumberOfCurrentPlayersDP: client_server_2.CMsgDPGetNumberOfCurrentPlayers,
+    EMsg.ClientGetNumberOfCurrentPlayersDPResponse: client_server_2.CMsgDPGetNumberOfCurrentPlayersResponse,
+    EMsg.ClientLogonGameServer: client_server_login.CMsgClientLogon,
+    EMsg.ClientCurrentUIMode: client_server_2.CMsgClientUIMode,
+    EMsg.ClientChatOfflineMessageNotification: client_server_2.CMsgClientOfflineMessageNotification,
+}
+
+cmsg_lookup = dict()
+
+for proto_module in [client_server, client_server_2, client_server_friends, client_server_login]:
+    cmsg_list = proto_module.__dict__
+    cmsg_list = fnmatch.filter(cmsg_list, 'CMsg*')
+    cmsg_lookup.update(dict(zip(map(lambda cmsg_name: cmsg_name.lower(), cmsg_list),
+                                map(lambda cmsg_name: getattr(proto_module, cmsg_name), cmsg_list))))
+
+
+def get_cmsg(emsg):
+    """Get protobuf for a given EMsg
+    :param emsg: EMsg
+    :type  emsg: :class:`steam.enums.emsg.EMsg`, :class:`int`
+    :return: protobuf message
+    """
+    if not isinstance(emsg, EMsg):
+        emsg = EMsg(emsg)
+
+    if emsg in cmsg_lookup_predefined:
+        return cmsg_lookup_predefined[emsg]
+    else:
+        enum_name = emsg.name.lower()
+        if enum_name.startswith("econ"):  # special case for 'EconTrading_'
+            enum_name = enum_name[4:]
+        cmsg_name = "cmsg" + enum_name
+
+    return cmsg_lookup.get(cmsg_name, None)
+
+
+class Msg:
+    proto = False
+    body = None  #: message instance
+    payload = None  #: Will contain body payload, if we fail to find correct message class
+
+    def __init__(self, msg, data=None, extended=False, parse=True):
+        self.extended = extended
+        self.header = ExtendedMsgHdr(data) if extended else MsgHdr(data)
+        self.msg = msg
+
+        if data:
+            self.payload = data[self.header._size:]
+
+        if parse:
+            self.parse()
+
+    def parse(self):
+        """Parses :attr:`payload` into :attr:`body` instance"""
+        if self.body is None:
+            deserializer = get_struct(self.msg)
+
+            if deserializer:
+                self.body = deserializer(self.payload)
+                self.payload = None
+            else:
+                self.body = '!!! Failed to resolve message !!!'
+
+    @property
+    def msg(self):
+        return self.header.msg
+
+    @msg.setter
+    def msg(self, value):
+        self.header.msg = EMsg(value)
+
+    @property
+    def steamID(self):
+        return self.header.steamID if isinstance(self.header, ExtendedMsgHdr) else None
+
+    @steamID.setter
+    def steamID(self, value):
+        if isinstance(self.header, ExtendedMsgHdr):
+            self.header.steamID = value
+
+    @property
+    def sessionID(self):
+        return self.header.sessionID if isinstance(self.header, ExtendedMsgHdr) else None
+
+    @sessionID.setter
+    def sessionID(self, value):
+        if isinstance(self.header, ExtendedMsgHdr):
+            self.header.sessionID = value
+
+    def serialize(self):
+        return self.header.serialize() + self.body.serialize()
+
+
+class MsgProto:
+    proto = True
+    body = None  #: protobuf message instance
+    payload = None  #: Will contain body payload, if we fail to find correct proto message
+
+    def __init__(self, msg, data=None, parse=True):
+        self._header = MsgHdrProtoBuf(data)
+        self.header = self._header.proto
+        self.msg = msg
+
+        if data:
+            self.payload = data[self._header._fullsize:]
+
+        if parse:
+            self.parse()
+
+    def parse(self):
+        """Parses :attr:`payload` into :attr:`body` instance"""
+        if self.body is None:
+            if self.msg in (EMsg.ServiceMethod, EMsg.ServiceMethodResponse, EMsg.ServiceMethodSendToClient):
+                is_resp = False if self.msg == EMsg.ServiceMethod else True
+                proto = get_um(self.header.target_job_name, response=is_resp)
+            else:
+                proto = get_cmsg(self.msg)
+
+            if proto:
+                self.body = proto()
+                if self.payload:
+                    self.body.ParseFromString(self.payload)
+                    self.payload = None
+            else:
+                self.body = '!!! Failed to resolve message !!!'
+
+    @property
+    def msg(self):
+        return self._header.msg
+
+    @msg.setter
+    def msg(self, value):
+        self._header.msg = EMsg(value)
+
+    @property
+    def steamID(self):
+        return self.header.steamid
+
+    @steamID.setter
+    def steamID(self, value):
+        self.header.steamid = value
+
+    @property
+    def sessionID(self):
+        return self.header.client_sessionid
+
+    @sessionID.setter
+    def sessionID(self, value):
+        self.header.client_sessionid = value
+
+    def serialize(self):
+        return self._header.serialize() + self.body.SerializeToString()
---
+++ steam/protobufs/structs.py
@@ -0,0 +1,358 @@
+"""Classes to (de)serialize various struct messages"""
+import struct
+
+import vdf
+
+from .emsg import EMsg
+from ..enums import EResult, EUniverse
+
+_emsg_map = dict()
+
+
+def get_struct(emsg):
+    return _emsg_map.get(emsg, None)
+
+
+class StructReader:
+    def __init__(self, data):
+        """Simplifies parsing of struct data from bytes
+        :param data: data bytes
+        :type  data: :class:`bytes`
+        """
+        if not isinstance(data, bytes):
+            raise ValueError("Only works with bytes")
+        self.data = data
+        self.offset = 0
+
+    def __len__(self):
+        return len(self.data)
+
+    def rlen(self):
+        """Number of remaining bytes that can be read
+        :return: number of remaining bytes
+        :rtype: :class:`int`
+        """
+        return max(0, len(self) - self.offset)
+
+    def read(self, n=1):
+        """Return n bytes
+        :param n: number of bytes to return
+        :type  n: :class:`int`
+        :return: bytes
+        :rtype: :class:`bytes`
+        """
+        self.offset += n
+        return self.data[self.offset - n:self.offset]
+
+    def read_cstring(self, terminator=b'\x00'):
+        """Reads a single null termianted string
+        :return: string without bytes
+        :rtype: :class:`bytes`
+        """
+        null_index = self.data.find(terminator, self.offset)
+        if null_index == -1:
+            raise RuntimeError("Reached end of buffer")
+        result = self.data[self.offset:null_index]  # bytes without the terminator
+        self.offset = null_index + len(terminator)  # advance offset past terminator
+        return result
+
+    def unpack(self, format_text):
+        """Unpack bytes using struct modules format
+        :param format_text: struct's module format
+        :type  format_text: :class:`str`
+        :return data: result from :func:`struct.unpack_from`
+        :rtype: :class:`tuple`
+        """
+        data = struct.unpack_from(format_text, self.data, self.offset)
+        self.offset += struct.calcsize(format_text)
+        return data
+
+    def skip(self, n):
+        """Skips the next ``n`` bytes
+        :param n: number of bytes to skip
+        :type  n: :class:`int`
+        """
+        self.offset += n
+
+
+class StructMessageMeta(type):
+    """Automatically adds subclasses of :class:`StructMessage` to the ``EMsg`` map"""
+
+    def __new__(mcs, name, bases, class_dict):
+        cls = type.__new__(mcs, name, bases, class_dict)
+
+        if name != 'StructMessage':
+            try:
+                _emsg_map[EMsg[name]] = cls
+            except KeyError:
+                pass
+
+        return cls
+
+
+class StructMessage(metaclass=StructMessageMeta):
+    def __init__(self, data=None):
+        if data:
+            self.load(data)
+
+    def serialize(self):
+        raise NotImplementedError
+
+    def load(self, data):
+        raise NotImplementedError
+
+
+class ChannelEncryptRequest(StructMessage):
+    protocolVersion = 1
+    universe = EUniverse.Invalid
+    challenge = b''
+
+    def serialize(self):
+        return struct.pack("<II", self.protocolVersion, self.universe) + self.challenge
+
+    def load(self, data):
+        (self.protocolVersion, universe) = struct.unpack_from("<II", data)
+
+        self.universe = EUniverse(universe)
+
+        if len(data) > 8:
+            self.challenge = data[8:]
+
+    def __str__(self):
+        return '\n'.join(["protocolVersion: %s" % self.protocolVersion,
+                          "universe: %s" % repr(self.universe),
+                          "challenge: %s" % repr(self.challenge),
+                          ])
+
+
+class ChannelEncryptResponse(StructMessage):
+    protocolVersion = 1
+    keySize = 128
+    key = ''
+    crc = 0
+
+    def serialize(self):
+        return struct.pack("<II128sII", self.protocolVersion, self.keySize, self.key, self.crc, 0)
+
+    def load(self, data):
+        (self.protocolVersion, self.keySize, self.key, self.crc, _) = struct.unpack_from("<II128sII", data)
+
+    def __str__(self):
+        return '\n'.join(["protocolVersion: %s" % self.protocolVersion,
+                          "keySize: %s" % self.keySize,
+                          "key: %s" % repr(self.key),
+                          "crc: %s" % self.crc,
+                          ])
+
+
+class ChannelEncryptResult(StructMessage):
+    eresult = EResult.Invalid
+
+    def serialize(self):
+        return struct.pack("<I", self.eresult)
+
+    def load(self, data):
+        (result,) = struct.unpack_from("<I", data)
+        self.eresult = EResult(result)
+
+    def __str__(self):
+        return "eresult: %s" % repr(self.eresult)
+
+
+class ClientLogOnResponse(StructMessage):
+    eresult = EResult.Invalid
+
+    def serialize(self):
+        return struct.pack("<I", self.eresult)
+
+    def load(self, data):
+        (result,) = struct.unpack_from("<I", data)
+        self.eresult = EResult(result)
+
+
+class ClientVACBanStatus(StructMessage):
+    class VACBanRange(object):
+        start = 0
+        end = 0
+
+        def __str__(self):
+            return '\n'.join(["{",
+                              "start: %s" % self.start,
+                              "end: %d" % self.end,
+                              "}",
+                              ])
+
+    @property
+    def numBans(self):
+        return len(self.ranges)
+
+    def __init__(self, data):
+        super().__init__(self)
+        self.ranges = list()
+
+    def load(self, data):
+        buf = StructReader(data)
+        numBans, = buf.unpack("<I")
+
+        for _ in range(numBans):
+            m = self.VACBanRange()
+            self.ranges.append(m)
+
+            m.start, m.end, _ = buf.unpack("<III")
+
+            if m.start > m.end:
+                m.start, m.end = m.end, m.start
+
+    def __str__(self):
+        text = ["numBans: %d" % self.numBans]
+
+        for m in self.ranges:  # emulate Protobuf text format
+            text.append("ranges " + str(m).replace("\n", "\n    ", 2))
+
+        return '\n'.join(text)
+
+
+class ClientChatMsg(StructMessage):
+    steamIdChatter = 0
+    steamIdChatRoom = 0
+    ChatMsgType = 0
+    text = ""
+
+    def serialize(self):
+        rbytes = struct.pack("<QQI", self.steamIdChatter, self.steamIdChatRoom, self.ChatMsgType)
+        # utf-8 encode only when unicode in py2 and str in py3
+        rbytes += (self.text.encode('utf-8') if isinstance(self.text, str) else self.text) + b'\x00'
+
+        return rbytes
+
+    def load(self, data):
+        buf = StructReader(data)
+        self.steamIdChatter, self.steamIdChatRoom, self.ChatMsgType = buf.unpack("<QQI")
+        self.text = buf.read_cstring().decode('utf-8')
+
+    def __str__(self):
+        return '\n'.join(["steamIdChatter: %d" % self.steamIdChatter,
+                          "steamIdChatRoom: %d" % self.steamIdChatRoom,
+                          "ChatMsgType: %d" % self.ChatMsgType,
+                          "text: %s" % repr(self.text),
+                          ])
+
+
+class ClientJoinChat(StructMessage):
+    steamIdChat = 0
+    isVoiceSpeaker = False
+
+    def serialize(self):
+        return struct.pack("<Q?", self.steamIdChat, self.isVoiceSpeaker)
+
+    def load(self, data):
+        (self.steamIdChat, self.isVoiceSpeaker) = struct.unpack_from("<Q?", data)
+
+
+class ClientChatMemberInfo(StructMessage):
+    steamIdChat = 0
+    type = 0
+    steamIdUserActedOn = 0
+    chatAction = 0
+    steamIdUserActedBy = 0
+
+    def serialize(self):
+        return struct.pack("<QIQIQ", self.steamIdChat, self.type, self.steamIdUserActedOn, self.chatAction,
+                           self.steamIdUserActedBy)
+
+    def load(self, data):
+        (self.steamIdChat,
+         self.type,
+         self.steamIdUserActedOn,
+         self.chatAction,
+         self.steamIdUserActedBy
+         ) = struct.unpack_from("<QIQIQ", data)
+
+    def __str__(self):
+        return '\n'.join(["steamIdChat: %d" % self.steamIdChat,
+                          "type: %r" % self.type,
+                          "steamIdUserActedOn: %d" % self.steamIdUserActedOn,
+                          "chatAction: %d" % self.chatAction,
+                          "steamIdUserActedBy: %d" % self.steamIdUserActedBy
+                          ])
+
+
+class ClientMarketingMessageUpdate2(StructMessage):
+    class MarketingMessage(object):
+        id = 0
+        url = ''
+        flags = 0
+
+    def __init__(self, data):
+        self.time = 0
+        self.messages = list()
+        StructMessage.__init__(self, data)
+
+    def load(self, data):
+        buf = StructReader(data)
+        self.time, count = buf.unpack("<II")
+
+        for _ in range(count):
+            m = self.MarketingMessage()
+            self.messages.append(m)
+
+            length, m.id = buf.unpack("<IQ")
+            m.url = buf.read_cstring().decode('utf-8')
+            m.flags = buf.unpack("<I")
+
+    def __str__(self):
+        text = ["time: %s" % self.time,
+                "count: %d" % self.count,
+                ]
+
+        for m in self.messages:  # emulate Protobuf text format
+            text.append("messages " + str(m).replace("\n", "\n    ", 3))
+
+        return '\n'.join(text)
+
+    @property
+    def count(self):
+        return len(self.messages)
+
+
+class ClientChatEnter(StructMessage):
+    steamIdChat = 0
+    steamIdFriend = 0
+    chatRoomType = 0
+    steamIdOwner = 0
+    steamIdClan = 0
+    chatFlags = 0
+    enterResponse = 0
+    numMembers = 0
+    chatRoomName = ""
+    memberList = []
+
+    def __init__(self, data=None):
+        super().__init__(data)
+        if data:
+            self.load(data)
+
+    def load(self, data):
+        buf, self.memberList = StructReader(data), list()
+
+        (self.steamIdChat, self.steamIdFriend, self.chatRoomType, self.steamIdOwner,
+         self.steamIdClan, self.chatFlags, self.enterResponse, self.numMembers
+         ) = buf.unpack("<QQIQQ?II")
+        self.chatRoomName = buf.read_cstring().decode('utf-8')
+
+        for _ in range(self.numMembers):
+            self.memberList.append(vdf.binary_loads(buf.read(64))['MessageObject'])
+
+        self.UNKNOWN1, = buf.unpack("<I")
+
+    def __str__(self):
+        return '\n'.join(["steamIdChat: %d" % self.steamIdChat,
+                          "steamIdFriend: %d" % self.steamIdFriend,
+                          "chatRoomType: %r" % self.chatRoomType,
+                          "steamIdOwner: %d" % self.steamIdOwner,
+                          "steamIdClan: %d" % self.steamIdClan,
+                          "chatFlags: %r" % self.chatFlags,
+                          "enterResponse: %r" % self.enterResponse,
+                          "numMembers: %r" % self.numMembers,
+                          "chatRoomName: %s" % repr(self.chatRoomName),
+                          ] + map(lambda x: "memberList: %s" % x, self.memberList))
