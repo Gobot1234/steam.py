ef078fd9bb25f9a76821abc1e71aea0ee17c9d44
James <gobot1234yt@gmail.com>
2020-04-11 20:39:27 +0100

    I think this will work

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import re
+import subprocess
 
 from setuptools import setup
 
@@ -13,15 +14,30 @@ with open('steam/__init__.py') as f:
 if version is None:
     raise RuntimeError('Version is not set')
 
+if version.endswith('+'):
+    # try to find out the commit hash if checked out from git, and append
+    # it to __version__ (since we use this value from setup.py, it gets
+    # automatically propagated to an installed copy as well)
+    version = version[:-1]  # remove '+' for PEP-440 version spec.
+    try:
+        ret = subprocess.run(['git', 'show', '-s', '--pretty=format:%h'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if ret.stdout:
+            version = f'{version}/{ret.stdout.decode("ascii").strip()}'
+    except Exception:
+        pass
+
+
 with open('README.md') as f:
     readme = f.read()
     f.close()
 
 extras_require = {
     'docs': [
-        'sphinx==1.8.5',
+        'sphinx==3.1.0',
         'sphinxcontrib_trio==1.1.0',
         'sphinxcontrib-websupport',
+        ''
     ]
 }
 
