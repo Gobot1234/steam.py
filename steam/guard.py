033d58877a743d47cbe2aa77350fca6efaa05ac8
James <gobot1234yt@gmail.com>
2020-04-15 19:31:13 +0100

    Fix confirmations

--- steam/guard.py
+++ steam/guard.py
@@ -38,10 +38,12 @@ import re
 import struct
 from hashlib import sha1
 from time import time
+from urllib.parse import urlencode
 
 from bs4 import BeautifulSoup
 
-from . import errors
+from . import utils
+from .errors import InvalidCredentials, AuthenticatorError
 from .models import URL
 
 log = logging.getLogger(__name__)
@@ -74,8 +76,8 @@ def generate_one_time_code(shared_secret: str, timestamp: int = int(time())) -> 
     return ''.join(code)  # faster than string concatenation
 
 
-def generate_confirmation_key(identity_secret: str, tag: str, timestamp: int = int(time())) -> str:
-    """Generate a trade confirmation key.
+def generate_confirmation_code(identity_secret: str, tag: str, timestamp: int = int(time())) -> str:
+    """Generate a trade confirmation code.
 
     Parameters
     -----------
@@ -88,19 +90,19 @@ def generate_confirmation_key(identity_secret: str, tag: str, timestamp: int = i
 
     Returns
     --------
-    :class:`bytes`
-        Confirmation key for the set timestamp.
+    :class:`str`
+        Confirmation code for the set timestamp.
     """
     buffer = struct.pack('>Q', timestamp) + tag.encode('ascii')
     return base64.b64encode(hmac.new(base64.b64decode(identity_secret), buffer, digestmod=sha1).digest()).decode()
 
 
-def generate_device_id(steam_id: str) -> str:
+def generate_device_id(id64: str) -> str:
     """
     Parameters
     -----------
-    steam_id: :class:`str`
-        The steam id to generate the id for
+    id64: :class:`str`
+        The 64 bit steam id to generate the device id for.
 
     Returns
     --------
@@ -109,7 +111,7 @@ def generate_device_id(steam_id: str) -> str:
     """
     # It works, however it's different that one generated from mobile app
 
-    hexed_steam_id = sha1(steam_id.encode('ascii')).hexdigest()
+    hexed_steam_id = sha1(id64.encode('ascii')).hexdigest()
     partial_id = [
         hexed_steam_id[:8],
         hexed_steam_id[8:12],
@@ -121,90 +123,105 @@ def generate_device_id(steam_id: str) -> str:
 
 
 def parse_token(url: str):
-    return re.search(r"https?://(?:www.)?steamcommunity.com/tradeoffer/new/\?partner=(?:\d+)(?:&|&amp;)"
-                     r"token=(?P<token>[a-zA-Z0-9-_]+)", url)
+    return re.search(r"https?://(?:www.)?steamcommunity.com/tradeoffer/new/\?partner=\d+(?:&|&amp;)"
+                     r"token=(?P<token>\d+)", url)
 
 
 class Confirmation:
-    def __init__(self, manager, state, offer_id, confirmation_id, data_key, creator):
-        self.manager = manager
-        self._state = state
-        self.id = int(offer_id.split('conf')[1])
-        self.confirmation_id = confirmation_id
+    def __init__(self, state, id, data_confid, data_key, manager, creator):
+        self.state = state
+        self.id = id.split('conf')[1]
+        self.data_confid = data_confid
         self.data_key = data_key
         self.tag = f'details{self.id}'
+        self.manager = manager
         self.creator = creator
 
+    def __repr__(self):
+        return f"<Confirmation id={self.id}>"
+
     def _confirm_params(self, tag):
+        timestamp = int(time())
         return {
-            'p': generate_device_id(self.manager.id64),
-            'a': self.manager.id,
-            'k': generate_confirmation_key(tag, self.manager.identity_secret),
-            't': int(time()),
+            'p': self.manager.device_id,
+            'a': str(self.manager.id64),
+            'k': self.manager.generate_confirmation(tag, timestamp),
+            't': timestamp,
             'm': 'android',
             'tag': tag
         }
 
-    async def confirm(self):
+    def confirm(self):
         params = self._confirm_params('allow')
         params['op'] = 'allow'
-        params['cid'] = self.confirmation_id
+        params['cid'] = self.data_confid
         params['ck'] = self.data_key
-        return await self._state.request('GET', url=f'{self.manager.BASE}/ajaxop', params=params)
+        return self.state.request('GET', f'{self.manager.BASE}/ajaxop', params=params)
 
-    async def cancel(self):
+    def cancel(self):
         params = self._confirm_params('cancel')
         params['op'] = 'cancel'
-        params['cid'] = self.confirmation_id
+        params['cid'] = self.data_confid
         params['ck'] = self.data_key
-        return await self._state.request('GET', url=f'{self.manager.BASE}/ajaxop', params=params)
+        return self.state.request('GET', f'{self.manager.BASE}/ajaxop', params=params)
 
-    async def details(self):
+    def details(self):  # need to do ['html'] for the good stuff
         params = self._confirm_params(self.tag)
-        resp = await self._state.request('GET', url=f'{self.manager.BASE}/details/{self.id}', params=params)
-        return resp['html']
+        return self.state.request('GET', f'{self.manager.BASE}/details/{self.id}', params=params)
 
 
 class ConfirmationManager:
-    BASE = f'{URL.COMMUNITY}/mobileconf/conf'
+    BASE = f'{URL.COMMUNITY}/mobileconf'
 
-    def __init__(self, state, id64):
-        self._state = state
+    def __init__(self, state):
+        self.state = state
         self.identity_secret = state.client.identity_secret
-        self.id64 = id64
+        self.id64 = state.client.user.id64
+        self.confirmations = []
 
-    def _create_confirmation_params(self, tag):
+    def create_confirmation_params(self, tag):
+        timestamp = int(time())
         return {
-            'p': generate_device_id(self.id64),
+            'p': self.device_id,
             'a': self.id64,
-            'k': generate_confirmation_key(self.identity_secret, tag),
-            't': int(time()),
+            'k': self.generate_confirmation(tag, timestamp),
+            't': timestamp,
             'm': 'android',
             'tag': tag
         }
 
     async def get_confirmations(self):
-        params = self._create_confirmation_params('conf')
+        params = self.create_confirmation_params('conf')
         headers = {'X-Requested-With': 'com.valvesoftware.android.steam.community'}
-        resp = await self._state.request('GET', f'{self.BASE}/conf', params=params, headers=headers)
-        if 'Oh nooooooes!' in resp:
-            raise errors.ConfirmationError
-        soup = BeautifulSoup(resp, 'html.parser')
+        confs = await self.state.request('GET', f'{self.BASE}/conf?{urlencode(params)}', headers=headers)
+
+        if 'incorrect Steam Guard codes.' in confs:
+            raise InvalidCredentials('identity secret is incorrect, or time is de-synced')
+        elif 'Oh nooooooes!' in confs:
+            return AuthenticatorError
+
+        soup = BeautifulSoup(confs, 'html.parser')
         if soup.select('#mobileconf_empty'):
             return []
-        to_confirm = []
         for confirmation in soup.select('#mobileconf_list .mobileconf_list_entry'):
-            offer_id = confirmation['id']
-            confirmation_id = confirmation['data-confid']
+            id = confirmation['id']
+            confid = confirmation['data-confid']
             key = confirmation['data-key']
             creator = confirmation.get('data-creator')
-            to_confirm.append(Confirmation(self, self._state, offer_id, confirmation_id, key, creator))
-        return to_confirm
+            self.confirmations.append(Confirmation(self.state, id, confid, key, self, creator))
+        return self.confirmations
 
-    async def get_trade_confirmation(self, trade_id, confirmations=None):
-        if confirmations is None:
-            confirmations = await self.get_confirmations()
-        for confirmation in confirmations:
-            if confirmation.creator == trade_id:
-                return confirmation
+    async def get_confirmation(self, id):
+        if id in [int(confirmation.creator) for confirmation in self.confirmations]:
+            return utils.find(lambda c: int(c.creator) == id, self.confirmations)
+        await self.get_confirmations()
+        if id in [int(confirmation.creator) for confirmation in self.confirmations]:
+            return utils.find(lambda c: int(c.creator) == id, self.confirmations)
         return None
+
+    @property
+    def device_id(self):
+        return generate_device_id(str(self.id64))
+
+    def generate_confirmation(self, tag, timestamp):
+        return generate_confirmation_code(self.identity_secret, tag, timestamp)
