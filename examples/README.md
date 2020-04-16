814a84583c679fe80ee601042cc7aed781755732
James <gobot1234yt@gmail.com>
2020-04-16 02:06:34 +0100

    Add comment monitor example

--- examples/README.md
+++ examples/README.md
@@ -8,5 +8,6 @@ More examples should be coming soon.
 
 <!-- - For a basic bot using ext.commands, see [basic.py](basic_bot.py). (spooky, this one isn't done yet) -->
 - For a basic trade framework that prints trades to stdout and auto-accepts gifts see [`trade.py`](trade.py). 
+- For an automatic suspicious comment deleter see [`comment_monitor.py`](comment_monitor.py)
 
 ###### Anyways that's all from me for now, Good luck and happy coding.
---
+++ examples/comment_monitor.py
@@ -0,0 +1,57 @@
+"""
+This example shows a simple comment section monitor
+if the comment is in the ClientUser's comments it will
+check if the comment's author is a friend, then if it contains
+a url then if so it will delete it.
+"""
+
+import re
+
+import steam
+
+client = steam.Client()
+
+# Please make sure that you do NOT ever share these,
+# you should be responsible for putting your own
+# measures in place to make sure no one apart
+# from you ever has access to these.
+
+username = ''
+password = ''
+shared_secret = ''
+
+URL_REGEX = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+
+# this regex matches any valid website url
+
+
+@client.event
+async def on_login():
+    async for comment in client.user.comments():  # check our comments since we last logged in
+        if comment.author in client.user.friends:  # ignore messages from friends
+            return
+
+        if re.search(URL_REGEX, comment.content):  # there is a url in the comment's content
+            await comment.delete()  # delete the comment
+            await comment.author.comment("Please don't post urls on my profile again.")
+            # finally warn them about posting urls in your comments
+            print('Deleted a suspicious comment containing a url from', comment.author.name)
+
+
+@client.event
+async def on_comment(comment: steam.Comment):
+    if comment.owner != client.user:  # if we don't own the section, don't watch the comments
+        return
+
+    if comment.author in client.user.friends:  # ignore messages from friends
+        return
+
+    if re.search(URL_REGEX, comment.content):  # there is a url in the comment's content
+        await comment.delete()  # delete the comment
+        await comment.author.comment("Please don't post urls on my profile again.")
+        # finally warn them about posting urls in your comments
+        print('Deleted a suspicious comment containing a url from', comment.author.name)
+
+
+client.run(username=username, password=password, shared_secret=shared_secret)
