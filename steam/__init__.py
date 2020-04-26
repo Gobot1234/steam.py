3f0af36e651652aa0674b106151f52b039351854
James <gobot1234yt@gmail.com>
2020-04-26 21:11:03 +0100

    That's not how that works

--- steam/__init__.py
+++ steam/__init__.py
@@ -5,7 +5,7 @@ steam.py
 ~~~~~~~~~~~~~
 A basic wrapper for the Steam API and its Community Managers.
 
-:copyright: (c) 2020
+:copyright: (c) 2020 Gobot1234
 :license: MIT, see LICENSE for more details.
 """
 
@@ -17,7 +17,7 @@ __version__ = '0.0.15a'
 import logging
 
 from . import guard, utils
-from .client import Client
+from .client import *
 from .enums import *
 from .errors import *
 from .game import *
