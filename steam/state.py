3b0505ad2711421b64c6a3615d428fd804365616
James <gobot1234yt@gmail.com>
2020-05-08 21:25:34 +0100

    Add get_and_confirm_confirmation method

--- steam/state.py
+++ steam/state.py
@@ -383,3 +383,12 @@ class ConnectionState:
 
     def _generate_confirmation(self, tag, timestamp):
         return generate_confirmation_code(self.client.identity_secret, tag, timestamp)
+
+    async def get_and_confirm_confirmation(self, id):
+        if state.client.identity_secret:
+            confirmation = self.get_confirmation(id) or await self.fetch_confirmation(id)
+            if confirmation is not None:
+                await confirmation.confirm()
+                return True
+
+        return False
