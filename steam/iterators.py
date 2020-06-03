10810ed4d4fbb1cdee398e7ecb48c0b125a4e086
James <gobot1234yt@gmail.com>
2020-06-03 19:43:04 +0100

    Re-order imports, document AsyncIterator

--- steam/iterators.py
+++ steam/iterators.py
@@ -27,32 +27,161 @@ SOFTWARE.
 import asyncio
 import re
 from datetime import datetime
-from typing import List, TYPE_CHECKING, Any
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    AsyncIterator as _AsyncIterator,
+    Awaitable,
+    Callable,
+    List,
+    Optional,
+    Union,
+)
 
 from bs4 import BeautifulSoup
 
 from . import utils
 from .enums import ETradeOfferState
-from .models import Comment, URL
+from .models import URL, Comment
 
 if TYPE_CHECKING:
+    from .abc import BaseUser
+    from .group import Group
     from .market import Listing
     from .trade import TradeOffer
     from .state import ConnectionState
 
+maybe_coro_predicate = Union[Callable[..., bool], Callable[..., Awaitable[bool]]]
 
-class AsyncIterator:
-    __slots__ = ('before', 'after', 'limit', 'queue', '_current_iteration', '_state')
+
+class AsyncIterator(_AsyncIterator):
+    """A class from which async iterators (see :pep:`525`) can ben easily derived.
+
+    .. container:: operations
+
+        .. describe:: async for x in y
+
+            Iterates over the contents of the async iterator.
+
+    Attributes
+    ----------
+    before: :class:`datetime.datetime`
+        When to find objects before.
+    after: :class:`datetime.datetime`
+        When to find objects after.
+    limit: Optional[:class:`int`]
+        The maximum size of the :attr:`AsyncIterator.queue`.
+    queue: :class:`asyncio.Queue`
+        The queue containing the elements of the iterator.
+    """
+    __slots__ = ('before', 'after', 'limit', 'queue', '_is_filled', '_state')
 
     def __init__(self, state: 'ConnectionState', limit: int, before: datetime, after: datetime):
         self._state = state
-        self.limit = limit
         self.before = before or datetime.utcnow()
         self.after = after or datetime.utcfromtimestamp(0)
-        self._current_iteration = 0
-        self.queue = asyncio.Queue()
+        self._is_filled = False
+        self.queue = asyncio.Queue(maxsize=limit)
+        self.limit = limit
+
+    def get(self, **attrs) -> Optional[Any]:
+        r"""A helper function which is similar to :func:`~steam.utils.get`
+        except it runs over the :class:`AsyncIterator.
+
+        This is roughly equipment to: ::
+
+            elements = await AsyncIterator.flatten()
+            element = steam.utils.get(name='Item', elements)
+
+
+        Example
+        -------
+        Getting the last comment from a user named 'Dave' or None: ::
+
+            msg = await User.comments().get(author__name='Dave')
+
+        Parameters
+        ----------
+        \*\*attrs
+            Keyword arguments that denote attributes to match.
+
+        Returns
+        -------
+        Optional[Any]
+            The first element from the ``iterable``
+            which matches all the traits passed in ``attrs``
+            or ``None`` if no matching element was found.
+        """
+
+        def predicate(elem):
+            for attr, val in attrs.items():
+                nested = attr.split('__')
+                obj = elem
+                for attribute in nested:
+                    obj = getattr(obj, attribute)
+
+                if obj != val:
+                    return False
+            return True
+
+        return self.find(predicate)
+
+    async def find(self, predicate: maybe_coro_predicate) -> Optional[Any]:
+        """|coro|
+        A helper function which is similar to :func:`~steam.utils.find`
+        except it runs over the :class:`AsyncIterator. However unlike
+         :func:`~steam.utils.find`, the predicate provided can be a |coroutine_link|_.
+
+        This is roughly equivalent to: ::
+
+            elements = await AsyncIterator.flatten()
+            element = steam.utils.find(elements, lambda e: e.name == 'Item')
+
+        Example
+        -------
+        Getting the last trade with a message or None: ::
+
+            def predicate(trade):
+                return trade.message is not None
+
+            trade = await Client.trade_history().find(predicate)
+
+        Parameters
+        ----------
+        predicate: Union[Callable[..., bool], Callable[..., Awaitable[bool]]]
+            A callable/coroutine that returns a boolean.
+
+        Returns
+        -------
+        Optional[Any]
+            The first element from the ``iterable``
+            for which the ``predicate`` returns ``True``
+            or ``None`` if no matching element was found.
+        """
+        while 1:
+            try:
+                elem = await self.next()
+            except StopAsyncIteration:
+                return None
+
+            ret = await utils.maybe_coroutine(predicate, elem)
+            if ret:
+                return elem
 
     async def flatten(self) -> List[Any]:
+        """|coro|
+        A helper function that iterates over the :class:`AsyncIterator`
+        returning a list of all the elements in the iterator.
+
+        This is roughly equivalent to: ::
+
+            elements = [async for element in AsyncIterator]
+
+        Returns
+        -------
+        List[Any]:
+            A list of every element in the iterator.
+        """
         ret = []
         while 1:
             try:
@@ -62,18 +191,23 @@ class AsyncIterator:
             else:
                 ret.append(item)
 
-    def __aiter__(self):
-        return self
-
     async def __anext__(self) -> Any:
         return await self.next()
 
     async def next(self) -> Any:
+        """|coro|
+        Advances the iterator by one, if possible.
+
+        Raises
+        --------
+        :exc:`StopAsyncIteration`:
+            There are no more elements in the iterator.
+        """
         if self.queue.empty():
-            self._current_iteration += 1
-            if self._current_iteration == 2:
+            if self._is_filled:
                 raise StopAsyncIteration
             await self.fill()
+            self._is_filled = True
         return self.queue.get_nowait()
 
     async def fill(self) -> None:
@@ -81,36 +215,38 @@ class AsyncIterator:
 
 
 class CommentsIterator(AsyncIterator):
-    __slots__ = ('owner', '_id', '_comment_type')
+    __slots__ = ('owner', '_id')
 
-    def __init__(self, state: 'ConnectionState', id: int, before: datetime,
-                 after: datetime, limit: int, comment_type: str):
+    def __init__(self, state: 'ConnectionState', before: datetime,
+                 after: datetime, limit: int, owner: Union['BaseUser', 'Group']):
         super().__init__(state, limit, before, after)
-        self._id = id
-        self._comment_type = comment_type
-        self.owner = None
+        self.owner = owner
 
     async def fill(self) -> None:
         from .user import User
 
-        data = await self._state.http.get_comments(id64=self._id, limit=self.limit, comment_type=self._comment_type)
-        self.owner = await self._state.fetch_user(self._id) if self._comment_type == 'Profile' else \
-            await self._state.client.fetch_group(self._id)
+        data = await self._state.http.get_comments(
+            id64=self.owner.id64, limit=self.limit,
+            comment_type='Profile' if isinstance(self.owner, User) else 'Clan'
+        )
         soup = BeautifulSoup(data['comments_html'], 'html.parser')
         comments = soup.find_all('div', attrs={'class': 'commentthread_comment responsive_body_text'})
         to_fetch = []
 
         for comment in comments:
-            if self.after < comment['data-timestamp'] < self.before:
-                timestamp = datetime.utcfromtimestamp(int(comment['data-timestamp']))
+            timestamp = datetime.utcfromtimestamp(int(comment['data-timestamp']))
+            if self.after < timestamp < self.before:
                 author_id = int(comment['data-miniprofile'])
                 comment_id = int(re.findall(r'comment_([0-9]*)', str(comment))[0])
-                content = comment.find('div', attrs={"class": 'commentthread_comment_text'}).text.strip()
+                content = comment.find('div', attrs={"class": 'commentthread_comment_text'}).get_text().strip()
                 to_fetch.append(utils.make_steam64(author_id))
-                self.queue.put_nowait(Comment(state=self._state, comment_type=self._comment_type,
-                                              comment_id=comment_id, timestamp=timestamp,
-                                              content=content, author=author_id, owner=self.owner))
-                if self.queue.qsize == self.limit:
+                comment = Comment(
+                    state=self._state, id=comment_id, timestamp=timestamp,
+                    content=content, author=author_id, owner=self.owner
+                )
+                try:
+                    self.queue.put_nowait(comment)
+                except asyncio.QueueFull:
                     return
         users = await self._state.http.get_profiles(to_fetch)
         for user in users:
@@ -119,7 +255,13 @@ class CommentsIterator(AsyncIterator):
                 if comment.author == author.id:
                     comment.author = author
 
-    async def flatten(self) -> List['Comment']:
+    def get(self, **attrs) -> Optional[Comment]:
+        return super().get(**attrs)
+
+    async def find(self, predicate: maybe_coro_predicate) -> Optional[Comment]:
+        return await super().find(predicate)
+
+    async def flatten(self) -> List[Comment]:
         return await super().flatten()
 
 
@@ -143,7 +285,7 @@ class TradesIterator(AsyncIterator):
                     if item['classid'] == asset['classid'] and item['instanceid'] == asset['instanceid']:
                         asset.update(item)
 
-            # patch in the attributes
+            # patch in the attributes cause steam is cool
             data['tradeofferid'] = data['tradeid']
             data['accountid_other'] = data['steamid_other']
             data['trade_offer_state'] = data['status']
@@ -151,13 +293,16 @@ class TradesIterator(AsyncIterator):
             data['items_to_receive'] = data.get('assets_received', [])
 
             trade = await TradeOffer._from_api(state=self._state, data=data)
-            if not self._active_only:
-                self.queue.put_nowait(trade)
-            elif self._active_only and trade.state in (ETradeOfferState.Active, ETradeOfferState.ConfirmationNeed):
-                self.queue.put_nowait(trade)
 
-            if self.queue.qsize() == self.limit:
-                raise StopAsyncIteration  # I think this is somewhat appropriate
+            try:
+                if not self._active_only:
+                    self.queue.put_nowait(trade)
+                elif self._active_only and trade.state in \
+                        (ETradeOfferState.Active, ETradeOfferState.ConfirmationNeed):
+                    self.queue.put_nowait(trade)
+
+            except asyncio.QueueFull:
+                raise StopAsyncIteration
 
     async def fill(self) -> None:
         resp = await self._state.http.get_trade_history(100, None)
@@ -187,6 +332,12 @@ class TradesIterator(AsyncIterator):
                     await self._process_trade(trade, descriptions)
         except StopAsyncIteration:
             return
+
+    def get(self, **attrs) -> Optional['TradeOffer']:
+        return super().get(**attrs)
+
+    async def find(self, predicate: maybe_coro_predicate) -> Optional['TradeOffer']:
+        return await super().find(predicate)
 
     async def flatten(self) -> List['TradeOffer']:
         return await super().flatten()
@@ -240,11 +391,17 @@ class MarketListingsIterator(AsyncIterator):
                             # this isn't very common, the listing couldn't be
                             # found I need to do more handling for this to make
                             # sure it doesn't happen but for now this will do.
-                            pass
-                        else:
+                            continue
+                        try:
                             self.queue.put_nowait(Listing(state=self._state, data=listing))
-                        if self.queue.qsize == self.limit:
+                        except asyncio.QueueFull:
                             return
+
+    def get(self, **attrs) -> Optional['Listing']:
+        return super().get(**attrs)
+
+    async def find(self, predicate: maybe_coro_predicate) -> Optional['Listing']:
+        return await super().find(predicate)
 
     async def flatten(self) -> List['Listing']:
         return await super().flatten()
