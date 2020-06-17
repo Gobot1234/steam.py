626ae2419f105f87a615c9144c0890ab9d0044f1
James <gobot1234yt@gmail.com>
2020-06-17 22:41:59 +0100

    More doc fixes

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-  var sections = $('div.section');
-  var activeLink = null;
-  var bottomHeightThreshold = $(document).height() - 30;
+  let sections = $('div.section');
+  let activeLink = null;
+  let bottomHeightThreshold = $(document).height() - 30;
 
   $(window).scroll(function (event) {
-    var distanceFromTop = $(this).scrollTop();
-    var currentSection = null;
+    let distanceFromTop = $(this).scrollTop();
+    let currentSection = null;
 
     if(distanceFromTop + window.innerHeight > bottomHeightThreshold) {
       currentSection = $(sections[sections.length - 1]);
     }
     else {
       sections.each(function () {
-        var section = $(this);
+        let section = $(this);
         if (section.offset().top - 1 < distanceFromTop) {
           currentSection = section;
         }
