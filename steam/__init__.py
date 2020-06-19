099c653b369c4b8b8637284f26bf4dddcaa3fbaf
James <gobot1234yt@gmail.com>
2020-06-19 23:55:32 +0100

    Bump version to 0.1.4

--- steam/__init__.py
+++ steam/__init__.py
@@ -12,21 +12,24 @@ A basic wrapper for the Steam API and its Community Managers.
 __title__ = 'steam'
 __author__ = 'Gobot1234'
 __license__ = 'MIT'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 
 import logging
 
 from . import guard, utils
 from .abc import *
+from .badge import *
 from .channel import *
 from .clan import *
 from .client import *
+from .comment import *
 from .enums import *
 from .errors import *
 from .game import *
 from .group import *
 from .image import *
+from .invite import *
 from .market import *
 from .message import *
 from .models import *
