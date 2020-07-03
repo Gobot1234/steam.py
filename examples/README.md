f3cf8f70002899713a9448c9e08927fef7606f33
James <gobot1234yt@gmail.com>
2020-07-03 20:14:47 +0100

    Add basic_bot.py

--- examples/README.md
+++ examples/README.md
@@ -6,8 +6,8 @@ can be found in [the documentation](https://steampy.rtfd.io/en/latest).
 
 More examples should be coming soon.
 
-<!-- - For a basic bot using ext.commands, see [basic_bot.py](basic_bot.py). (spooky, this one isn't done yet) -->
-- For a basic bot that automatically replies to messages, see [`reply.py`](reply.py).
+- For a basic bot using ext.commands, see [`basic_bot.py`](basic_bot.py).
+- For a client that automatically replies to messages, see [`reply.py`](reply.py).
 - For a basic trade framework that prints trades to stdout and auto-accepts gifts see [`trade.py`](trade.py). 
 - For an automatic suspicious comment deleter see [`comment_monitor.py`](comment_monitor.py)
 
---
+++ examples/basic_bot.py
@@ -0,0 +1,54 @@
+import asyncio
+import random
+
+from steam.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print('------------')
+    print('Logged in as')
+    print('Username:', bot.user)
+    print('ID:', bot.user.id64)
+    print('Friends:', len(bot.user.friends))
+    print('------------')
+
+
+@bot.command()
+async def trade(ctx):
+    await ctx.send(f'Send me a trade {ctx.author}!')
+
+    def check(trade):
+        return trade.partner == ctx.author
+
+    try:
+        offer = await bot.wait_for('trade_receive', timeout=60, check=check)
+    except asyncio.TimeoutError:
+        await ctx.send('You took too long to send the offer')
+    else:
+        await ctx.send(f'You were going to send {len(offer.items_to_receive)} items\n'
+                       f'You were going to receive {len(offer.items_to_receive)} items')
+        await offer.decline()
+
+
+@bot.command()
+async def roll(ctx, sides: int = 6, rolls: int = 1):
+    """Roll a dice."""
+    result = ', '.join(str(random.randint(1, sides)) for _ in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command()
+async def choose(ctx, *choices):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! My latency is {bot.latency * 1000:2f}')
+
+
+bot.run('username', 'password')
