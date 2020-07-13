bbb0a49f15e19671685f6b3d869ff2df39641b44
James <gobot1234yt@gmail.com>
2020-07-13 18:03:08 +0100

    Bump version to 0.3.0

--- steam/__init__.py
+++ steam/__init__.py
@@ -12,7 +12,7 @@ A basic wrapper for the Steam API and its Community Managers.
 __title__ = "steam"
 __author__ = "Gobot1234"
 __license__ = "MIT"
-__version__ = "0.2.6"
+__version__ = "0.3.0"
 
 
 import logging
@@ -44,6 +44,6 @@ class VersionInfo(NamedTuple):
     release_level: str
 
 
-version_info = VersionInfo(major=0, minor=2, macro=6, release_level="full")
+version_info = VersionInfo(major=0, minor=3, macro=0, release_level="full")
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
