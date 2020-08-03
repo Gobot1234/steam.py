d2ac7fbbac3394d60d9ee7481fad891f0f1b38a6
James <gobot1234yt@gmail.com>
2020-08-03 11:25:37 +0100

    Doc fixes

--- docs/conf.py
+++ docs/conf.py
@@ -7,12 +7,15 @@
 import os
 import re
 import sys
+import typing
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.append(os.path.abspath("extensions"))
+
+typing.TYPE_CHECKING = True
 
 # -- General configuration ------------------------------------------------
 
@@ -87,12 +90,6 @@ language = None
 locale_dirs = ["locale/"]
 gettext_compact = False
 
-# There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-# today = ''
-# Else, today_fmt is used as the format for a strftime call.
-# today_fmt = '%B %d, %Y'
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ["_build"]
@@ -100,22 +97,12 @@ exclude_patterns = ["_build"]
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "friendly"
 
-# A list of ignored prefixes for module index sorting.
-# modindex_common_prefix = []
-
-# If true, keep warnings as "system message" paragraphs in the built documents.
-# keep_warnings = False
-
 
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "basic"
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-# html_logo = None
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -130,47 +117,6 @@ html_static_path = ["_static"]
 # The name of a javascript file (relative to the configuration directory) that
 # implements a search results scorer. If empty, the default will be used.
 html_search_scorer = "_static/scorer.js"
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = "steam.pydoc"
-
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    ("index", "steam.py.tex", "steam.py Documentation", "Gobot1234", "manual"),
-]
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [("index", "steam.py", "steam.py Documentation", ["Gobot1234"], 1)]
-
-# If true, show URL addresses after external links.
-# man_show_urls = False
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (
-        "index",
-        "steam.py",
-        "steam.py Documentation",
-        "Gobot1234",
-        "steam.py",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
-]
 
 
 def setup(app):
