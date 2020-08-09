869fd53f65c2b53e6b85ffb8b0f4c31e8f3bb338
James <gobot1234yt@gmail.com>
2020-08-09 20:55:02 +0100

    Add init

---
+++ tests/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import os
+from . import test_steam_id
+
+"""
+USER_1_ID64: str = os.getenv()
+USERNAME_1: str = os.getenv()
+PASSWORD_1: str = os.getenv()
+SHARED_SECRET_1: str = os.getenv()
+IDENTITY_SECRET_1: str = os.getenv()
+
+USER_2_ID64: str = os.getenv()
+USERNAME_2: str = os.getenv()
+PASSWORD_2: str = os.getenv()
+SHARED_SECRET_2: str = os.getenv()
+IDENTITY_SECRET_2: str = os.getenv()
+"""
