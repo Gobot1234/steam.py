969fc34bb8586b3de6cd2e12be615fd3f79b04d6
James <gobot1234yt@gmail.com>
2020-09-12 23:58:01 +0100

    Fix failing docs

--- docs/extensions/events.py
+++ docs/extensions/events.py
@@ -4,11 +4,26 @@
 # TYPE_CHECKING block to be able to be picked up by Sphinx.
 
 import importlib
+import inspect
 
-from steam.ext.commands import utils
+import steam
+from steam.ext import commands
 
 client = importlib.import_module("steam.client")
 bot = importlib.import_module("steam.ext.commands.bot")
 
-utils.reload_module_with_TYPE_CHECKING(client)
-utils.reload_module_with_TYPE_CHECKING(bot)
+commands.utils.reload_module_with_TYPE_CHECKING(client)
+commands.utils.reload_module_with_TYPE_CHECKING(bot)
+
+OLD_GETATTR_STATIC = inspect.getattr_static
+
+
+def getattr_static(obj, attr, default=inspect._sentinel):
+    if obj is steam.Client:
+        return OLD_GETATTR_STATIC(client.Client, attr, default)
+    elif obj is commands.Bot:
+        return OLD_GETATTR_STATIC(bot.Bot, attr, default)
+    return OLD_GETATTR_STATIC(obj, attr, default)
+
+
+inspect.getattr_static = getattr_static
