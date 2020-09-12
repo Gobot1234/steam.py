10b90f5ac575f2213ecf15debe622efb0cccbf52
James <gobot1234yt@gmail.com>
2020-09-13 00:33:09 +0100

    Perhaps?

--- docs/conf.py
+++ docs/conf.py
@@ -8,6 +8,8 @@ import os
 import re
 import sys
 
+from extensions import events
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
