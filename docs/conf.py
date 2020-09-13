e2b5e683aab5a998d929b79f1262105dc50d40f0
James <gobot1234yt@gmail.com>
2020-09-13 02:26:04 +0100

    Fix events not being documented properly

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinxcontrib_trio",
     "exception_hierarchy",
+    "enum",
     "events",
 ]
 
--- docs/extensions/events.py
+++ docs/extensions/events.py
@@ -4,12 +4,6 @@
 # TYPE_CHECKING block to be able to be picked up by Sphinx.
 
 import importlib
-import inspect as _inspect
-from typing import Any
-
-from sphinx.application import Sphinx
-from sphinx.util import inspect
-from sphinx.ext import autodoc
 
 import steam
 from steam.ext import commands
@@ -20,26 +14,9 @@ bot = importlib.import_module("steam.ext.commands.bot")
 commands.utils.reload_module_with_TYPE_CHECKING(client)
 commands.utils.reload_module_with_TYPE_CHECKING(bot)
 
-OLD_AUTODOC_ATTRGETTER = autodoc.autodoc_attrgetter
-OLD_GETATTR_STATIC = _inspect.getattr_static
 
-
-def autodoc_attrgetter(app: Sphinx, obj: Any, name: str, *defargs: Any) -> Any:
-    """Alternative getattr() for types"""
-    if obj is steam.Client:
-        return inspect.safe_getattr(client.Client, name, *defargs)
-    elif obj is commands.Bot:
-        return inspect.safe_getattr(bot.Bot, name, *defargs)
-    return OLD_AUTODOC_ATTRGETTER
-
-
-def getattr_static(obj, attr, default=_inspect._sentinel):
-    if obj is steam.Client:
-        return OLD_GETATTR_STATIC(client.Client, attr, default)
-    elif obj is commands.Bot:
-        return OLD_GETATTR_STATIC(client.Bot, attr, default)
-    return OLD_GETATTR_STATIC(obj, attr, default)
-
-
-autodoc.autodoc_attrgetter = autodoc_attrgetter
-_inspect.getattr_static = getattr_static
+def setup(_):
+    steam.client = client
+    steam.Client = client.Client
+    commands.bot = bot
+    commands.Bot = bot.Bot
