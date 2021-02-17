724585ee02f8bd7e097e1598ed59df2697236962
gobot1234 <gobot1234yt@gmail.com>
2021-02-17 18:24:29 +0000

    Ok this has to work right?

--- docs/conf.py
+++ docs/conf.py
@@ -156,4 +156,4 @@ def setup(app: Sphinx) -> None:
 
         from docs.extensions.minimizer import minimize
 
-        app.connect("builder-init", minimize)
+        app.connect("builder-inited", minimize)
