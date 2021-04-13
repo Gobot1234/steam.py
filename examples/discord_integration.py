251f471f4ff7175ed36a287a2c407f202e2b13fa
James <50501825+Gobot1234@users.noreply.github.com>
2021-04-13 19:25:35 +0100

    Improve the example

--- examples/discord_integration.py
+++ examples/discord_integration.py
@@ -1,4 +1,5 @@
-from typing import TYPE_CHECKING, Optional
+import asyncio
+from typing import TYPE_CHECKING
 
 import discord  # pip install discord.py
 from discord.ext import commands
@@ -20,14 +21,18 @@ class DiscordBot(commands.Bot):
         print("Ready")
 
     async def start(self, token: str, username: str, password: str) -> None:
-        self.loop.create_task(
-            self.client.start(username, password)
-        )  # start the client in a task to avoid blocking the discord bot's startup
-        await super().start(token)
+        await asyncio.gather(
+            super().start(token),
+            self.client.start(username, password),
+        )  # start the client and bot concurrently
 
     async def close(self) -> None:
         await self.client.close()  # make sure to close the client when we close the discord bot
         await super().close()
+
+
+class UserNotFound(commands.CommandError):
+    """For when a matching user cannot be found"""
 
 
 class UserConverter(commands.Converter):
@@ -35,16 +40,19 @@ class UserConverter(commands.Converter):
 
     async def convert(self, ctx: commands.Context, argument: str) -> Optional[steam.User]:
         try:
-            return await ctx.bot.client.fetch_user(argument)
+            user = await ctx.bot.client.fetch_user(argument)
         except steam.InvalidSteamID:
             id64 = await steam.utils.id64_from_url(argument)
             if id64 is None:
-                return
-            await ctx.bot.client.fetch_user(id64)
+                raise commands.BadArgument
+            user = await ctx.bot.client.fetch_user(id64)
+        if user is None:
+            raise UserNotFound
+        return user
 
 
 if TYPE_CHECKING:
-    UserConverter = Optional[steam.User]  # make linters play nicely with d.py's converters
+    UserConverter = steam.User  # make linters play nicely with d.py's converters
 
 
 bot = DiscordBot()
@@ -53,9 +61,6 @@ bot = DiscordBot()
 @bot.command()
 async def user(ctx: commands.Context, user: UserConverter):
     """Show some basic info on a steam user"""
-    if user is None:
-        return await ctx.send("User not found")
-
     embed = discord.Embed(description=user.name, timestamp=user.created_at)
     embed.set_thumbnail(url=user.avatar_url)
     embed.add_field(name="64 bit ID:", value=str(user.id64))
@@ -66,4 +71,10 @@ async def user(ctx: commands.Context, user: UserConverter):
     await ctx.send(f"Info on {user.name}", embed=embed)
 
 
-bot.run("token", "username", "password")
+@user.error
+async def on_user_command_error(ctx: Context, error: commands.CommandError):
+    if isinstance(error, UserNotFound):
+        return await ctx.send("User not found")
+    raise error
+
+bot.run("discord_token", "username", "password")
