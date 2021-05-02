de5cbc31ad93de840e9de27116b25eae00c6b913
James <50501825+Gobot1234@users.noreply.github.com>
2021-05-03 00:41:53 +0100

    Maybe the problem is here?

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,4 @@ USERNAME: str = os.getenv("USERNAME")
 PASSWORD: str = os.getenv("PASSWORD")
 SHARED_SECRET: str = os.getenv("SHARED_SECRET")
 IDENTITY_SECRET: str = os.getenv("IDENTITY_SECRET")
-RUNNING_AS_ACTION: bool = os.getenv("GITHUB_ACTIONS") == "TRUE"
+RUNNING_AS_ACTION: bool = os.getenv("GITHUB_ACTIONS").lower() == "true"
