cae572910cd845f7943a85587432ad3452dc1984
James <50501825+Gobot1234@users.noreply.github.com>
2021-05-03 00:22:25 +0100

    Update test_basic.py
    Strange fact of the day tuple.__lt__ supports arbitrary length tuple comparison but __eq__ doesn't?

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -52,7 +52,7 @@ class Client(steam.Client):
 
 
 @pytest.mark.skipif(
-    not USERNAME if not RUNNING_AS_ACTION else sys.version_info == (3, 8),
+    not USERNAME if not RUNNING_AS_ACTION else sys.version_info.minor == 8,  # 3.8
     reason="If there are issues they are normally present in one of the 2 versions, "
     "as well, it will ask for a CAPTCHA code if you login twice simultaneously on the third computer",
 )
