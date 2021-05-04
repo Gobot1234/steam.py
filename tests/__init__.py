0472a3a0dea92ab8aa3abeb928f06287ae4f7140
James <gobot1234yt@gmail.com>
2021-05-04 22:41:44 +0100

    Fix an AttributeError

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,4 @@ USERNAME: str = os.getenv("USERNAME")
 PASSWORD: str = os.getenv("PASSWORD")
 SHARED_SECRET: str = os.getenv("SHARED_SECRET")
 IDENTITY_SECRET: str = os.getenv("IDENTITY_SECRET")
-RUNNING_AS_ACTION: bool = os.getenv("GITHUB_ACTIONS").lower() == "true"
+RUNNING_AS_ACTION: bool = os.getenv("GITHUB_ACTIONS", "").lower() == "true"
