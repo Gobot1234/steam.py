e259af280ef29cdf1cdee8b8c0c370a5d4e07ab8
James Hilton-Balfe <50501825+Gobot1234@users.noreply.github.com>
2021-09-05 13:45:41 +0100

    Apply suggestions from code review

--- steam/user.py
+++ steam/user.py
@@ -180,8 +180,10 @@ class User(BaseUser, Messageable["UserMessage"]):
             to_receive = [item.to_dict() for item in trade.items_to_receive]
             resp = await self._state.http.send_trade_offer(self, to_send, to_receive, trade.token, trade.message or "")
             trade._has_been_sent = True
-            trade._update_from_send(self._state, resp, self, active=not resp.get("needs_mobile_confirmation", False))
-            if resp.get("needs_mobile_confirmation", False):
+            needs_confirmation = resp.get("needs_mobile_confirmation", False)
+            trade._update_from_send(self._state, resp, self)
+            trade._update_from_send(self._state, resp, self, active=not needs_confirmation)
+            if needs_confirmation:
                 for tries in range(5):
                     try:
                         await trade.confirm()
@@ -189,7 +191,7 @@ class User(BaseUser, Messageable["UserMessage"]):
                         break
                     except ClientException:
                         await asyncio.sleep(tries * 2)
-                trade._update_from_send(self._state, resp, self)
+                trade.state = TradeOfferState.Active
 
         return message
 
