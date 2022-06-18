f9935ce4514b06aa01449286c45be56c1f4992cd
James Hilton-Balfe <gobot1234yt@gmail.com>
2022-06-18 11:23:28 +0100

    Add poe scripts

---
+++ scripts/add_cms.py
@@ -0,0 +1,51 @@
+"""Licensed under The MIT License (MIT) - Copyright (c) 2020-present James H-B. See LICENSE"""
+# A script to add the default list of CMs to the constants file if Steam is down.
+
+import asyncio
+from pathlib import Path
+
+import aiohttp
+import black
+from typing_extensions import Final
+
+from steam import NoCMsFound
+from steam.models import api_route
+
+ROOT: Final = Path(__file__).parent
+DEFAULT_CMS_URL: Final = api_route("ISteamDirectory/GetCMListForConnect") % {
+    "cmtype": "websockets",
+    "realm": "steamglobal",
+}
+CONST_FILE: Final = ROOT.parent / "steam" / "_const.py"
+
+
+async def amain() -> None:
+    async with aiohttp.ClientSession() as client:
+        async with client.get(DEFAULT_CMS_URL) as r:
+            default_cms = await r.json(encoding="UTF-8")
+
+    resp = default_cms["response"]
+    if not resp["success"]:
+        raise NoCMsFound(f"No Community Managers could be found to connect to:\n{resp['message']!r}")
+
+    ws_list: list[str] = sorted(cm["endpoint"] for cm in default_cms["response"]["serverlist"])
+
+    with CONST_FILE.open("r") as fp:
+        new_lines = list[str]()
+        for line in fp:
+            new_lines.append(line)
+            if line.strip() == "# default CMs if Steam API is down":
+                break
+
+        new_lines.append(f"DEFAULT_CMS: Final = {black.format_str(str(tuple(ws_list)), mode=black.Mode())}")
+
+    with CONST_FILE.open("w") as fp:
+        fp.write("".join(new_lines))
+
+
+def main() -> None:
+    asyncio.run(amain())
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/decode.py
@@ -0,0 +1,45 @@
+"""Licensed under The MIT License (MIT) - Copyright (c) 2020-present James H-B. See LICENSE"""
+# A small script that imitates the behaviour of the WS parser and allows you to debug messages from the steam chat
+
+import asyncio
+import sys
+from base64 import b64decode
+from collections import defaultdict
+from pprint import pprint
+
+import betterproto
+
+import steam
+from steam.gateway import SteamWebSocket
+from steam.protobufs import EMsg, MsgBase, MsgProto
+from steam.protobufs.base import CMsgMulti
+
+
+async def amain(input_message: str) -> None:
+    client = steam.Client()
+    client.http.user = steam.SteamID()  # type: ignore
+    fake_ws = SteamWebSocket(client._connection, None, None, None)  # type: ignore
+
+    def parser(msg: MsgBase[betterproto.Message]) -> None:
+        print(f"{msg.msg=}")
+        pprint(msg.body)
+        if msg.body._unknown_fields:
+            print(f"Unknown fields: {msg.body._unknown_fields}")
+
+    async def handle_multi(msg: MsgProto[CMsgMulti]) -> None:
+        print("This is a multi message, unpacking...")
+        pprint(msg.body)
+        await fake_ws.handle_multi(msg)
+
+    fake_ws.parsers = defaultdict(lambda: parser)
+    fake_ws.parsers[EMsg.Multi] = handle_multi
+    await fake_ws.receive(b64decode(input_message))
+    await asyncio.sleep(2)
+
+
+def main(input: str) -> None:
+    return asyncio.run(amain(input))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
