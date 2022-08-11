0858e326677e3a631d55fc9384d88b9c1fe9e475
James Hilton-Balfe <gobot1234yt@gmail.com>
2022-08-11 20:57:19 +0100

    Update CI versions and add back integration tests

---
+++ tests/integration/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+USERNAME: str = os.getenv("USERNAME")
+PASSWORD: str = os.getenv("PASSWORD")
+SHARED_SECRET: str = os.getenv("SHARED_SECRET")
+IDENTITY_SECRET: str = os.getenv("IDENTITY_SECRET")
+RUNNING_AS_ACTION: bool = os.getenv("GITHUB_ACTIONS", "").lower() == "true"
