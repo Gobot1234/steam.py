20b9969523b7f247fb082a0d7b7502354114ccca
lnfecteDru <nikitasmn98@gmail.com>
2023-07-20 12:47:21 +0300

    Fixed wrong metal addition

--- steam/ext/tf2/currency.py
+++ steam/ext/tf2/currency.py
@@ -3,26 +3,38 @@
 from __future__ import annotations
 
 import math
+from decimal import Decimal
 from fractions import Fraction
-from typing import Final, SupportsFloat, SupportsIndex, SupportsRound, TypeAlias, overload
+from typing import TypeAlias, overload
 
 from typing_extensions import Self
 
 __all__ = ("Metal",)
 
 
-class SupportsRoundOrFloat(SupportsRound[int], SupportsFloat):
-    ...
+SupportsMetal: TypeAlias = int | str | float | Fraction | Decimal
 
 
-class SupportsRoundOrIndex(SupportsRound[int], SupportsIndex):
-    ...
+def modf(value: Decimal) -> tuple[Decimal, Decimal]:
+    # can't just use divmod(value, 1) as both raise for large Decimals because lol,
+    # so we need this whole song and dance
+    as_tuple = value.as_tuple()
+    if not isinstance(as_tuple.exponent, int):
+        raise ValueError(f"invalid exponent {as_tuple.exponent!r}")
+
+    if not as_tuple.exponent:
+        integer = value
+        fractional = Decimal()
+    elif as_tuple.exponent < 0:
+        integer = Decimal((as_tuple.sign, as_tuple.digits[: as_tuple.exponent], 0))
+        fractional = Decimal((as_tuple.sign, as_tuple.digits[as_tuple.exponent :], as_tuple.exponent))
+    else:
+        raise AssertionError("shouldn't be reachable")
+
+    return fractional, integer
 
 
-SupportsMetal: TypeAlias = float | str | SupportsRoundOrFloat | SupportsRoundOrIndex
-
-
-# TODO look into the discuss post about returning Self() in subclasses instead of concrete instances
+# TODO use https://github.com/python/cpython/pull/106223 when it's merged
 class Metal(Fraction):
     """A class to represent some metal in TF2.
 
@@ -36,6 +48,10 @@ class Metal(Fraction):
         Metal("1.22") == Metal(1.22)
         Metal(1.22) == Metal(1.22)
         Metal(1) == Metal(1.00)
+
+    Note
+    ----
+    When working with large floating point numbers, it's recommended to pass a :class:`str` to avoid precision loss.
     """
 
     __slots__ = ()
@@ -45,32 +61,57 @@ class Metal(Fraction):
         ...
 
     def __new__(cls, value: SupportsMetal, /, *, _normalize: bool = ...) -> Self:
-        if isinstance(value, str):  # '1.22'
-            value = float(value)
+        scrap = cls.extract_scrap(value)
 
-        self = object.__new__(cls)
+        if scrap < 0:
+            raise ValueError(f'Metal value cannot be negative')
+
+        return super().__new__(cls, scrap, 9)
+    
+    @classmethod
+    def extract_scrap(cls, value: SupportsMetal) -> int:
+        if isinstance(value, Fraction):
+            if value.denominator in {9, 3, 1}:
+                return value.numerator * (9 // value.denominator)
+            raise ValueError("cannot convert Fraction to Metal, denominator isn't 1, 3 or 9")
 
         try:
-            true_value = round(value, 2)
+            value = Decimal(value)
         except (ValueError, TypeError):
             raise TypeError("non-int passed to Metal.__new__, that could not be cast") from None
 
-        if not math.isclose(value, true_value):
+        fractional, integer = modf(value)
+        if not math.isclose(fractional - round(fractional, 2), 0, abs_tol=1e-9):
             raise ValueError("metal value's last digits must be close to 0")
 
-        stred = f"{true_value:.2f}"
-        if stred[-1] != stred[-2]:
+        digits = round(fractional, 2).as_tuple().digits
+        if len(digits) >= 2 and digits[0] != digits[1]:
             raise ValueError("metal value must be a multiple of 0.11")
+        
+        return int(integer) * 9 + digits[0]
+    
+    def __add__(self, other: SupportsMetal) -> Fraction:
+        scrap = Metal.extract_scrap(other)
+        return Metal(Fraction(self.numerator + scrap, 9))
+    
+    def __sub__(self, other: SupportsMetal) -> Fraction:
+        scrap = Metal.extract_scrap(other)
+        return Metal(Fraction(self.numerator - scrap, 9))
+    
+    def __mul__(self, other: SupportsMetal) -> Fraction:
+        scrap = Metal.extract_scrap(other)
+        return Metal(Fraction(self.numerator * scrap, 9))
+    
+    def __div__(self, other: SupportsMetal) -> Fraction:
+        scrap = Metal.extract_scrap(other)
 
-        self._numerator = round(true_value * 9)
-        return self
-
-    denominator: Final = 9  # type: ignore
-    _numerator: int
-    _denominator: Final = 9
+        if self.numerator % scrap != 0:
+            raise ValueError(f'{self.numerator} scrap cannot be divided by {scrap}')
+        
+        return Metal(Fraction(self.numerator // scrap, 9))
 
     def __str__(self) -> str:
-        return f"{self._numerator / 9:.2f}"
+        return f"{self.numerator // self.denominator}.{f'{(self.numerator % self.denominator) * 9 // self.denominator}' * 2}"
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({str(self)})"
+        return f"{self.__class__.__name__}({str(self)!r})"
--- tests/unit/test_ext_tf2.py
+++ tests/unit/test_ext_tf2.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from fractions import Fraction
+
 import pytest
 
 from steam.ext import tf2
@@ -5,8 +8,35 @@ from steam.ext import tf2
 client = tf2.Client()
 
 
+def test_metal_initializations():
+    assert tf2.Metal("0.77") == tf2.Metal(0.77)
+    assert tf2.Metal(Fraction(7, 9)) == tf2.Metal(0.77)
+    assert tf2.Metal(Decimal(0.77)) == tf2.Metal(0.77)
+    assert tf2.Metal(Fraction(18, 9)) == tf2.Metal(2)
+    assert tf2.Metal(1.00) == tf2.Metal(1)
+
+
 def test_metal_addition():
     assert tf2.Metal(1.11) + tf2.Metal(1) == tf2.Metal(2.11)
+    assert tf2.Metal(1.88) + tf2.Metal(1.11) == tf2.Metal(3)
+    assert tf2.Metal(1.11) + tf2.Metal(1.11) == tf2.Metal(2.22)
+    assert tf2.Metal(1.88) + tf2.Metal(1.88) == tf2.Metal(3.77)
+
+
+def test_metal_subtraction():
+    assert tf2.Metal(1.11) - tf2.Metal(1) == tf2.Metal(0.11)
+    assert tf2.Metal(1.11) - tf2.Metal(0) == tf2.Metal(1.11)
+
+    with pytest.raises(ValueError):
+        _ = tf2.Metal(1) - tf2.Metal(1.11)
+
+
+def test_metal_multiplication():
+    assert tf2.Metal(3) * 3 == tf2.Metal(9)
+    assert tf2.Metal(0) * 3 == tf2.Metal(0)
+
+    with pytest.raises(ValueError):
+        _ = tf2.Metal(3) * -1
 
 
 def test_metal_invalid_values():
