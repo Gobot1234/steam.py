f8836258c3867114fd057ab3d416e52a4c9b81d4
James Hilton-Balfe <gobot1234yt@gmail.com>
2023-07-20 11:51:51 +0100

    Improve tests with hypothesis

--- tests/unit/test_ext_tf2.py
+++ tests/unit/test_ext_tf2.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from fractions import Fraction
 
 import pytest
+from hypothesis import given, strategies as st
 
 from steam.ext import tf2
 
@@ -14,7 +15,14 @@ def test_metal_initializations():
     assert tf2.Metal(Fraction(7, 9)) == tf2.Metal(0.77)
     assert tf2.Metal(Decimal(0.77)) == tf2.Metal(0.77)
     assert tf2.Metal(Fraction(18, 9)) == tf2.Metal(2)
-    assert tf2.Metal(1.00) == tf2.Metal(1)
+    assert tf2.Metal(1.11) + tf2.Metal(1) == tf2.Metal(2.11)
+    assert tf2.Metal(1.88) + tf2.Metal(1.11) == tf2.Metal(3)
+    assert tf2.Metal(1.11) + tf2.Metal(1.11) == tf2.Metal(2.22)
+    assert tf2.Metal(1.88) + tf2.Metal(1.88) == tf2.Metal(3.77)
+    assert tf2.Metal(1.00) + tf2.Metal(0) == tf2.Metal(1)
+
+    assert tf2.Metal(1.99) == tf2.Metal(2)
+    # TODO test inf prec
 
 
 def test_metal_addition():
@@ -60,3 +68,19 @@ def test_metal_invalid_values():
         tf2.Metal(1.115)
     with pytest.raises(ValueError):
         tf2.Metal(Fraction(1, 10))
+
+def test_str():
+    assert str(tf2.Metal(1.22)) == "1.22"
+    assert str(tf2.Metal(1.33)) == "1.33"
+    assert str(tf2.Metal(1000)) == "1000.00"
+
+
+# some property tests
+@given(st.integers(), st.integers())
+def test_ints_are_commutative(x: int, y: int):
+    assert x + y == y + x
+
+
+@given(x=st.integers(), y=st.integers())
+def test_ints_cancel(x: int, y: int):
+    assert (tf2.Metal(x) + tf2.Metal(y)) - tf2.Metal(y) == tf2.Metal(x)
