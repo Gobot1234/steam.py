24ef0e2e70eb8982462a0007a7d360f6841eedeb
Eugene Lopatkin <mailofdante@gmail.com>
2023-10-10 17:55:54 +0300

    Fix for ClientHello message (#365)
    Co-authored-by: Eugene Lopatkin <eugenelopatkin@gmail.com>

--- steam/_gc/client.py
+++ steam/_gc/client.py
@@ -77,7 +77,7 @@ class Client(Client_):
         await self._state.login_complete.wait()
         while not self.is_closed():
             for base in self._GC_BASES:
-                if msg := base._get_gc_message(self._state):
+                if (msg := base._get_gc_message(self._state)) is not None:
                     await self._state.ws.send_gc_message(msg)
             await asyncio.sleep(self._GC_HEART_BEAT)
 
